3c2ba2665a94b75827c109f554297ac41da9d5fe
AS <AS@gmail.com>
2016-07-26 17:30:22 +0300

    1. Proje oluşturuldu.
    2. Tema giydirildi.
    3. Login sayfası hazırlandı.

---
+++ Membership.Web/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Membership.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost,HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+            return new JsonResult() { };
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            return new JsonResult() { };
+        }
+
+
+        public class LoginViewModel
+        {
+            public string email { get; set; }
+            public string password { get; set; }
+            public bool rememberMe { get; set; }
+            public string returnUrl{ get; set; }
+        }
+
+
+        [HttpPost, HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel model)
+        {
+            var sss = model;
+
+            return new JsonResult() { };
+        }
+
+        //public ActionResult Login()
+        //{
+
+        //    return new JsonResult() {  };
+        //}
+    }
+}
